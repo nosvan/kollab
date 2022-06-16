f399b8ea79d8f2ea9cec773fe628d828ecf4e2a6
nos <>
2022-06-16 19:40:29 -0400

    templating for classes and groups page and modify gitignore

---
+++ .next/server/pages/api/auth/user.js
@@ -0,0 +1,138 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 873;
+exports.ids = [873];
+exports.modules = {
+
+/***/ 14:
+/***/ ((module) => {
+
+module.exports = import("iron-session");;
+
+/***/ }),
+
+/***/ 527:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "d": () => (/* binding */ sessionOptions)
+/* harmony export */ });
+const sessionOptions = {
+    password: process.env.JWT_SECRET,
+    cookieName: "iron-session-token",
+    cookieOptions: {
+        maxAge: 60 * 60 * 24,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: "production" === "production",
+        path: "/"
+    }
+};
+
+
+/***/ }),
+
+/***/ 533:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ lib_prisma)
+});
+
+;// CONCATENATED MODULE: external "@prisma/client"
+const client_namespaceObject = require("@prisma/client");
+;// CONCATENATED MODULE: ./lib/prisma.ts
+
+let prisma;
+if (true) {
+    prisma = new client_namespaceObject.PrismaClient();
+} else {}
+/* harmony default export */ const lib_prisma = (prisma);
+
+
+/***/ }),
+
+/***/ 349:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _lib_prisma__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(533);
+/* harmony import */ var _lib_iron_session__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(527);
+/* harmony import */ var iron_session_next__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(534);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([iron_session_next__WEBPACK_IMPORTED_MODULE_2__]);
+iron_session_next__WEBPACK_IMPORTED_MODULE_2__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,iron_session_next__WEBPACK_IMPORTED_MODULE_2__/* .withIronSessionApiRoute */ .n)(handle, _lib_iron_session__WEBPACK_IMPORTED_MODULE_1__/* .sessionOptions */ .d));
+async function handle(req, res) {
+    const { cookies  } = req;
+    const t0k3n = cookies["iron-session-token"];
+    const user = {
+        id: -999,
+        first_name: "",
+        last_name: "",
+        email: "",
+        isLoggedIn: false
+    };
+    if (!t0k3n) {
+        console.log("no token");
+        res.status(401).json(user);
+    }
+    try {
+        console.log("verifying session token");
+        const userFromSession = await req.session.user;
+        const result = await _lib_prisma__WEBPACK_IMPORTED_MODULE_0__/* ["default"].user.findUnique */ .Z.user.findUnique({
+            select: {
+                id: true,
+                first_name: true,
+                last_name: true,
+                email: true,
+                password: true
+            },
+            where: {
+                email: userFromSession.email
+            }
+        });
+        if (userFromSession.password === result?.password) {
+            const safeUser = {
+                id: result.id,
+                first_name: result.first_name,
+                last_name: result.last_name,
+                email: result.email,
+                isLoggedIn: true
+            };
+            console.log("verified session token");
+            res.status(200).json(safeUser);
+        } else {
+            res.status(200).json(user);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(401).json(user);
+    }
+}
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [534], () => (__webpack_exec__(349)));
+module.exports = __webpack_exports__;
+
+})();
