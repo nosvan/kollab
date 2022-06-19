edf31e6251ceb8c9a1c61f57b4afa8631df32a20
nos <>
2022-06-19 12:31:29 -0400

    xd2

--- .next/server/pages/api/group/new.js
+++ .next/server/pages/api/group/new.js
@@ -33,6 +33,28 @@ const sessionOptions = {
 
 /***/ }),
 
+/***/ 533:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ lib_prisma)
+});
+
+;// CONCATENATED MODULE: external "@prisma/client"
+const client_namespaceObject = require("@prisma/client");
+;// CONCATENATED MODULE: ./lib/prisma.ts
+
+let prisma;
+if (true) {
+    prisma = new client_namespaceObject.PrismaClient();
+} else {}
+/* harmony default export */ const lib_prisma = (prisma);
+
+
+/***/ }),
+
 /***/ 379:
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
@@ -43,43 +65,50 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var iron_session_next__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(534);
 /* harmony import */ var lib_iron_session__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(527);
+/* harmony import */ var lib_prisma__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(533);
 var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([iron_session_next__WEBPACK_IMPORTED_MODULE_0__]);
 iron_session_next__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
 
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,iron_session_next__WEBPACK_IMPORTED_MODULE_0__/* .withIronSessionApiRoute */ .n)(handle, lib_iron_session__WEBPACK_IMPORTED_MODULE_1__/* .sessionOptions */ .d));
 async function handle(req, res) {
     if (req.method === "POST") {
         const newGroupData = req.body;
-    // try {
-    //   const result = await prisma.group.create({
-    //     select: {
-    //       id: true,
-    //       name: true,
-    //       owner_id: true,
-    //       passcode: true,
-    //       created_at: true,
-    //     },
-    //     data: {
-    //       name: newGroupData.name,
-    //       description: newGroupData.description,
-    //       owner_id: req.session.user.id,
-    //       passcode: newGroupData.passcode
-    //   }})
-    //   console.log(result)
-    //   const safeResult = {
-    //     id: result.id,
-    //     name: result.name,
-    //     owner_id: result.owner_id,
-    //     created_at: result.created_at
-    //   }
-    //   console.log(safeResult)
-    //   res.json(safeResult)
-    // } catch (error) {
-    //   res.json(error)
-    // }
+        try {
+            const result = await lib_prisma__WEBPACK_IMPORTED_MODULE_2__/* ["default"].group.create */ .Z.group.create({
+                select: {
+                    id: true,
+                    name: true,
+                    description: true,
+                    owner_id: true,
+                    passcode: true,
+                    created_at: true
+                },
+                data: {
+                    name: newGroupData.name,
+                    description: newGroupData.description,
+                    owner_id: req.session.user.id,
+                    passcode: newGroupData.passcode
+                }
+            });
+            console.log(result);
+            const safeResult = {
+                id: result.id,
+                name: result.name,
+                owner_id: result.owner_id,
+                created_at: result.created_at
+            };
+            res.json(safeResult);
+        } catch (error) {
+            res.json(error);
+        }
     }
-}
+} // id         Int      @id @default(autoincrement())
+ // name       String
+ // description String
+ // owner_id   Int
+ // passcode   String
 
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } });
--- pages/api/group/new.ts
+++ pages/api/group/new.ts
@@ -10,32 +10,38 @@ export default withIronSessionApiRoute(handle, sessionOptions)
 async function handle(req: NextApiRequest,res: NextApiResponse){
   if(req.method === 'POST'){
     const newGroupData: GroupRegister = req.body;
-    // try {
-    //   const result = await prisma.group.create({
-    //     select: {
-    //       id: true,
-    //       name: true,
-    //       owner_id: true,
-    //       passcode: true,
-    //       created_at: true,
-    //     },
-    //     data: {
-    //       name: newGroupData.name,
-    //       description: newGroupData.description,
-    //       owner_id: req.session.user.id,
-    //       passcode: newGroupData.passcode
-    //   }})
-    //   console.log(result)
-    //   const safeResult = {
-    //     id: result.id,
-    //     name: result.name,
-    //     owner_id: result.owner_id,
-    //     created_at: result.created_at
-    //   }
-    //   console.log(safeResult)
-    //   res.json(safeResult)
-    // } catch (error) {
-    //   res.json(error)
-    // }
+    try {
+      const result = await prisma.group.create({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          owner_id: true,
+          passcode: true,
+          created_at: true,
+        },
+        data: {
+          name: newGroupData.name,
+          description: newGroupData.description,
+          owner_id: req.session.user.id,
+          passcode: newGroupData.passcode
+      }})
+      console.log(result)
+      const safeResult = {
+        id: result.id,
+        name: result.name,
+        owner_id: result.owner_id,
+        created_at: result.created_at
+      }
+      res.json(safeResult)
+    } catch (error) {
+      res.json(error)
+    }
   } 
 }
+
+// id         Int      @id @default(autoincrement())
+// name       String
+// description String
+// owner_id   Int
+// passcode   String
