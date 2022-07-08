29e152ad84add8549dee929d93dbdc81d146e355
nos <>
2022-07-07 21:03:52 -0400

    delete migrations

--- prisma/migrations/20220707190738_v2/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "classroom" ALTER COLUMN "description" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "group" ALTER COLUMN "description" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "item" ALTER COLUMN "description" DROP NOT NULL,
-ALTER COLUMN "category_id" DROP NOT NULL;
